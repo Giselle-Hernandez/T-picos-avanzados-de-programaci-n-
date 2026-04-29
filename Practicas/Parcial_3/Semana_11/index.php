9b7cc84e4f819d083ab1d8eda4e747fd85ea7e7b
Giselle-Hernandez <hernandezgiselleguadalupe@gmail.com>
2026-04-28 22:35:45 -0600

    commit del codigo ya cometado

--- Practicas/Parcial_3/Semana_11/index.php
+++ Practicas/Parcial_3/Semana_11/index.php
@@ -1,57 +1,58 @@
-<?php
-$usuario = "Estudiante";
-$fecha_hoy = date("d / m / Y");
-$saldo_total = 1250.50;
+<?php //inicia codigo php
+$usuario = "Vanessa"; // crea la variable de usuario y le asigna el valor de "vanessa"
+$fecha_hoy = date("d / m / Y"); // en la variable de fecha guarda la fecha actual en un formato de dia mes y año
+$saldo_total = 1250.50; // vrea la variable de saldo_total y le asigna un valor 
 
-$transacciones = [
-    ["item" => "Mensualidad Gym", "monto" => -450.00, "categoria" => "Salud"],
-    ["item" => "Pago salario", "monto" => 1500.00, "categoria" => "Ingreso"],
-    ["item" => "Comida fuera", "monto" => -320.50, "categoria" => "Comida"],
-    ["item" => "Transporte", "monto" => -12.00, "categoria" => "Viajes"]
-];
+$transacciones = [ //crea un arreglo de nombre transacciones
+    ["item" => "Mensualidad Gym", "monto" => -450.00, "categoria" => "Salud"], //item es igal al nombre del gasto
+    ["item" => "Pago de salario", "monto" => 1500.00, "categoria" => "Ingreso"], //el monto es la cantidad de dinero
+    ["item" => "Comida fuera", "monto" => -320.50, "categoria" => "Comida"], //categoria es el tipo de gasto
+    ["item" => "Transporte", "monto" => -12.00, "categoria" => "Viajes"] // la flecha de => signifoca clave apuntando al valor
+]; //cierra el arreglo 
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi Cartera App</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
+<!DOCTYPE html> <!--indica que inicia codigo html -->
+<html lang="es"> <!--indica que esta escrito en español -->
+<head><!--abre el encabezado -->
+    <meta charset="UTF-8"> <!--linea para que acepte los caracteres especiales -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--es la linea para que sea responsive -->     
+    <title>Mi Cartera App</title><!--es el titulo de la pestaña-->
+    <link rel="stylesheet" href="styles.css"><!--es el enlace con la hoja de estilos -->
+</head> <!--cierra el encabezado -->
 
-<header>
-    <div class="container">
-        <p>Hola, <strong><?php echo $usuario; ?></strong> 👋</p>
-        <span class="balance-label">Saldo disponible</span>
-        <div class="balance-amount">$<?php echo number_format($saldo_total, 2); ?></div>
-        <p class="fecha"><?php echo $fecha_hoy; ?></p>
-    </div>
-</header>
+<body><!--abre el cuerpo -->
 
-<main class="container">
-    <h3>Movimientos recientes</h3>
-    
-    <div class="card transaction-list">
-        <?php foreach($transacciones as $t): ?>
-            <div class="item">
-                <div>
-                    <div class="item-title"><?php echo $t['item']; ?></div>
-                    <small class="item-category"><?php echo $t['categoria']; ?></small>
+    <header> <!--es para mostrar datos en la parte superior de la pagina -->
+        <div class="container"> <!--crea un contendor y le asigna una clase de nombre container -->
+            <p>Hola, <strong><?php echo $usuario; ?></strong></p> <!--este es el mensaje que se muestra en la pantalla del usuario -->
+            <span class="balance-label">Saldo disponible</span> <!--en esta linea se utiliza una etiqueta uqe crea una pequeña cajita y se le asigna una clase de nombre balance-label y tiene un texto que dice saldo dosponible  -->
+            <div class="balance-amount">$<?php echo number_format($saldo_total, 2); ?></div> <!--crea un contenedor y le asigna una clase de nombre balance-amount  para darle formato al texto, despues muestra codigo php que se esta utilizando para darle un formato al numero-->
+            <p class="fecha"><?php echo $fecha_hoy; ?></p><!--muestra en un parrafo  -->
+        </div>
+    </header>
+
+    <main class="container">
+        <h3>Movimientos recientes</h3> <!--este es un titulo de tamaño 3 -->
+        
+        <div class="hoja de transacciones">  <!--crea un contenedor para dar formato a la siguiente informacion -->
+            <?php foreach($transacciones as $t): ?><!--indica codigo php y es un ciclo que dice que recorra el arreglo una por una de las transacciones y haga lo siguente -->
+                <div class="item"><!--crea un cotenedor para dar formatos -->
+                    <div>
+                        <div class="item-title"><?php echo $t['item']; ?></div><!--crea un contenedor para dar formato y muestra el nombre del gasto -->
+                        <small class="item-category"><?php echo $t['categoria']; ?></small> <!--en pequeño muestra la categoria a la que pertenece -->
+                    </div><!--cierra el contenedor -->
+                    <div class="item-amount" 
+                        style="color: <?php echo $t['monto'] < 0 ? '#ef4444' : '#10b981'; ?>;"> <!--color de los numeros-->
+                        <?php echo ($t['monto'] < 0 ? '-' : '+') . ' $' . abs($t['monto']); ?> <!--pone un signo antes dependiendo si es un gasto o un ingreso-->
+                    </div>
                 </div>
-                <div class="item-amount"
-                     style="color: <?php echo $t['monto'] < 0 ? '#ef4444' : '#10b981'; ?>;">
-                    <?php echo ($t['monto'] < 0 ? '-' : '+') . ' $' . abs($t['monto']); ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+            <?php endforeach; ?>
+        </div>
 
-    <button class="btn-action" onclick="alert('Gasto agregado')">
-        + Agregar Nuevo Gasto
-    </button>
-</main>
+        <button class="btn-action" onclick="alert('Gasto agregado')"> <!--crea un boton para hacer la accion-->
+            + Agregar Nuevo Gasto <!-- texto del boton-->
+        </button>
+    </main><!--es la etiqueta del tipo "desarrollo" -->
 
 </body>
 </html>
